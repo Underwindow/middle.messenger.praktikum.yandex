c2757fa8f21ec914a45772c2eadcaee1e3d248d1
Underwindow <genya@podoinikov.ru>
2022-11-05 01:00:53 +0500

    Minor changes

--- src/pages/error-page/errorPage.ts
+++ src/pages/error-page/errorPage.ts
@@ -1,12 +1,10 @@
 import './errorPage.css';
 import { Block, CoreRouter, Store } from 'core';
-import { ErrorProps } from 'components/app-error/error';
 import { Screens, withRouter, withStore } from 'utils';
 
 type ErrorPageProps = {
     router: CoreRouter,
     store: Store<AppState>,
-    error: ErrorProps,
     text?: string,
     onClick?: Callback,
 };
@@ -14,34 +12,24 @@ type ErrorPageProps = {
 export class ErrorPage extends Block<ErrorPageProps> {
     static readonly componentName = 'ErrorPage';
 
-    static readonly CODES = {
-        404: {
-            code: '404',
-            description: 'Не туда попали',
-        } as ErrorProps,
-        505: {
-            code: '5**',
-            description: 'Что-то пошло не так',
-        } as ErrorProps,
-    };
-
-    constructor({ error = ErrorPage.CODES[404], ...props }: ErrorPageProps) {
+    constructor({...props }: ErrorPageProps) {
         super({
             ...props,
-            error,
             text: 'Вернуться на главную',
             onClick: () => this.props.router.go(Screens.Messenger),
         });
     }
 
     protected render() {
+        const state = this.props.store.getState();
         // language=hbs
         return `
         <div class="screen content-center back-panel">
             <div class="error-container">
-            {{#with error}}
-                {{{Error code=code description=description}}}
-            {{/with}}
+                {{{Error 
+                    code="${state.apiError?.status ?? 404}" 
+                    description="${state.apiError?.reason ?? 'Не туда попали'}"
+                }}}
                 {{{ButtonSecondary onClick=onClick type="button" text=text}}}
             </div>    
         </div>
